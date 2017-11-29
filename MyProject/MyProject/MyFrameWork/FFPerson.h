5c10b80f393969a27366ddd3e030468ea87509d2
dou <13521567956@163.com>
2017-11-29 10:36:59 +0800

    框架开发完成

---
+++ MyProject/MyProject/MyFrameWork/FFPerson.h
@@ -0,0 +1,16 @@
+//
+//  FFPerson.h
+//  MyProject
+//
+//  Created by dou on 2017/11/29.
+//  Copyright © 2017年 dcl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFPerson : NSObject
+
+@property (nonatomic,copy) NSString *name;
+
+- (void)eat;
+@end
---
+++ MyProject/MyProject/MyFrameWork/FFPerson.m
@@ -0,0 +1,16 @@
+//
+//  FFPerson.m
+//  MyProject
+//
+//  Created by dou on 2017/11/29.
+//  Copyright © 2017年 dcl. All rights reserved.
+//
+
+#import "FFPerson.h"
+
+@implementation FFPerson
+
+- (void)eat {
+    NSLog(@"%@ 正在吃...",_name);
+}
+@end
